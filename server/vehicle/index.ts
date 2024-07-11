2094faa2915a8f0dfbe3008451d58310995245f5
Cyril Beeckman <cyril.beeckman@gmail.com>
2024-07-11 08:48:00 +0200

    our modif

--- server/vehicle/index.ts
+++ server/vehicle/index.ts
@@ -153,39 +153,20 @@ export async function SpawnVehicle(id: number, coords: number | number[], headin
 	return await CreateVehicle(vehicle, coords, heading, invokingScript);
 }
 
-function object_equals(x: any, y: any) {
-	if (x === y) return true;
-	// if both x and y are null or undefined and exactly the same
-
-	if (!(x instanceof Object) || !(y instanceof Object)) return false;
-	// if they are not strictly equal, they both need to be Objects
-
-	if (x.constructor !== y.constructor) return false;
-	// they must have the exact same prototype chain, the closest we can do is
-	// test there constructor.
-
-	for (var p in x) {
-		if (!x.hasOwnProperty(p)) continue;
-		// other properties were tested using x.constructor === y.constructor
-
-		if (!y.hasOwnProperty(p)) return false;
-		// allows to compare x[ p ] and y[ p ] when set to undefined
-
-		if (x[p] === y[p]) continue;
-		// if they have the same strict value or identity then they are equal
-
-		if (typeof (x[p]) !== "object") return false;
-		// Numbers, Strings, Functions, Booleans must be strictly equal
-
-		if (!object_equals(x[p], y[p])) return false;
-		// Objects and Arrays must be tested recursively
+function doesfivemworkyet(obj1: any, obj2: any) {
+	for (let key in obj1) {
+		if (obj1.hasOwnProperty(key)) {
+			if (typeof obj1[key] === 'object' && obj1[key] !== null) {
+				if (!doesfivemworkyet(obj1[key], obj2[key])) {
+					return false;
+				}
+			} else {
+				if (obj2[key] !== obj1[key]) {
+					return false;
+				}
+			}
+		}
 	}
-
-	for (p in y)
-		if (y.hasOwnProperty(p) && !x.hasOwnProperty(p))
-			return false;
-	// allows x[ p ] to be set to undefined
-
 	return true;
 }
 
@@ -202,7 +183,7 @@ setInterval(async () => {
 						// test if the values have changed and return keys that have changed
 						const changedKeys = []
 						for (const key in properties) {
-							if (!object_equals(currentProperties[key], properties[key])) {
+							if (!doesfivemworkyet(currentProperties[key], properties[key])) {
 								changedKeys.push(key);
 							}
 						}
