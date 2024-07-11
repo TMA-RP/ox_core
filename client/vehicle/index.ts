bd2073f5ed12c14f774eef4a9c3ae85faba57333
Cyril Beeckman <cyril.beeckman@gmail.com>
2024-07-11 12:48:51 +0200

    our modif

--- client/vehicle/index.ts
+++ client/vehicle/index.ts
@@ -1,4 +1,11 @@
-import { cache, onServerCallback, setVehicleProperties, sleep } from '@overextended/ox_lib/client';
+import {
+	cache,
+	getVehicleProperties,
+	onServerCallback,
+	setVehicleProperties,
+	waitFor,
+	sleep
+} from '@overextended/ox_lib/client';
 import { Vector3 } from '@nativewrappers/fivem';
 import { DEBUG } from '../config';
 
@@ -20,19 +27,86 @@ onServerCallback('ox:getNearbyVehicles', (radius: number) => {
 
 AddStateBagChangeHandler('initVehicle', '', async (bagName: string, key: string, value: any) => {
 	if (!value) return;
-	const netId = parseInt(bagName.split(':')[1]);
-	if (!NetworkDoesEntityExistWithNetworkId(netId)) return;
-	const vehicle = NetworkGetEntityFromNetworkId(netId);
-	SetVehicleOnGroundProperly(vehicle);
-	setTimeout(() => Entity(vehicle).state.set(key, null, true));
+
+	const entity = await waitFor(async () => {
+		const entity = GetEntityFromStateBagName(bagName);
+		DEV: console.info(key, entity);
+
+		if (entity) return entity;
+	}, 'failed to get entity from statebag name');
+
+	if (!entity) return;
+
+	try {
+		const success = await waitFor(async () => {
+			if (!IsEntityWaitingForWorldCollision(entity)) return true;
+		});
+		if (!success) return;
+	} catch (e) {
+		return;
+	}
+
+	if (NetworkGetEntityOwner(entity) !== cache.playerId) return;
+
+	SetVehicleOnGroundProperly(entity);
+	setTimeout(() => Entity(entity).state.set(key, null, true));
 });
 
+function doesfivemworkyet(obj1: any, obj2: any) {
+	for (let key in obj1) {
+		if (obj1.hasOwnProperty(key)) {
+			if (typeof obj1[key] === 'object' && obj1[key] !== null) {
+				if (!doesfivemworkyet(obj1[key], obj2[key])) {
+					return false;
+				}
+			} else {
+				if (obj2[key] !== obj1[key]) {
+					return false;
+				}
+			}
+		}
+	}
+	return true;
+}
+
 AddStateBagChangeHandler('vehicleProperties', '', async (bagName: string, key: string, value: any) => {
-	if (!value) return;
-	const netId = parseInt(bagName.split(':')[1]);
-	if (!NetworkDoesEntityExistWithNetworkId(netId)) return;
-	const vehicle = NetworkGetEntityFromNetworkId(netId);
-	if (setVehicleProperties(vehicle, value)) Entity(vehicle).state.set(key, null, true);
+	if (!value) return DEBUG && console.info(`removed ${key} state from ${bagName}`);
+
+	const entity = await waitFor(async () => {
+		const entity = GetEntityFromStateBagName(bagName);
+		DEV: console.info(key, entity);
+
+		if (entity) return entity;
+	}, 'failed to get entity from statebag name');
+
+	if (!entity) return;
+
+	// properties and serverside vehicles are one of the most retarded features of fivem
+	// let's set this dumb bullshit in an interval and see if they actually bother setting
+	const resolved = await new Promise((resolve) => {
+		let i = 0;
+		let interval: CitizenTimer;
+
+		interval = setInterval(() => {
+			i++;
+
+			if (i > 10 || !DoesEntityExist(entity)) {
+				resolve(i > 10 ? 1 : 0);
+				return clearInterval(interval);
+			}
+
+			setVehicleProperties(entity, value);
+		}, 100);
+	});
+
+	if (!resolved) return;
+
+	const properties = getVehicleProperties(entity);
+
+	if (!doesfivemworkyet(value, properties))
+		console.error(`vehicle properties probably didn't fully set properly. thanks fivem.`);
+
+	Entity(entity).state.set(key, null, true);
 });
 
 onNet('ox_core:vehicle:enter', async (netId: number) => {
