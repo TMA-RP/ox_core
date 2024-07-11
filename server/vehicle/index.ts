30d96b48c73ab9252b0c1f14ceeb6daae4395ec5
Cyril Beeckman <cyril.beeckman@gmail.com>
2024-07-11 07:45:14 +0200

    our modif

--- server/vehicle/index.ts
+++ server/vehicle/index.ts
@@ -153,6 +153,42 @@ export async function SpawnVehicle(id: number, coords: number | number[], headin
 	return await CreateVehicle(vehicle, coords, heading, invokingScript);
 }
 
+function object_equals(x, y) {
+	if (x === y) return true;
+	// if both x and y are null or undefined and exactly the same
+
+	if (!(x instanceof Object) || !(y instanceof Object)) return false;
+	// if they are not strictly equal, they both need to be Objects
+
+	if (x.constructor !== y.constructor) return false;
+	// they must have the exact same prototype chain, the closest we can do is
+	// test there constructor.
+
+	for (var p in x) {
+		if (!x.hasOwnProperty(p)) continue;
+		// other properties were tested using x.constructor === y.constructor
+
+		if (!y.hasOwnProperty(p)) return false;
+		// allows to compare x[ p ] and y[ p ] when set to undefined
+
+		if (x[p] === y[p]) continue;
+		// if they have the same strict value or identity then they are equal
+
+		if (typeof (x[p]) !== "object") return false;
+		// Numbers, Strings, Functions, Booleans must be strictly equal
+
+		if (!object_equals(x[p], y[p])) return false;
+		// Objects and Arrays must be tested recursively
+	}
+
+	for (p in y)
+		if (y.hasOwnProperty(p) && !x.hasOwnProperty(p))
+			return false;
+	// allows x[ p ] to be set to undefined
+
+	return true;
+}
+
 setInterval(async () => {
 	for (const key of Object.keys(OxVehicle.getAll())) {
 		const vehicle = OxVehicle.get(key);
@@ -166,12 +202,16 @@ setInterval(async () => {
 						// test if the values have changed and return keys that have changed
 						const changedKeys = []
 						for (const key in properties) {
-							if (currentProperties[key] !== properties[key]) {
+							if (!object_equals(currentProperties[key], properties[key])) {
 								changedKeys.push(key);
 							}
 						}
 						if (changedKeys.length > 0) {
+							console.log("####################################################")
 							console.log(`[ceeb_debug] Vehicle ${vehicle.id} properties changed: ${changedKeys.join(', ')}`);
+							changedKeys.forEach((key) => {
+								console.log(`[ceeb_debug] ${key}: ${currentProperties[key]} -> ${properties[key]}`);
+							})
 						}
 						vehicle.set('properties', properties);
 					}
